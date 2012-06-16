9b4d54ed71f5da3ed047c87c3c1f6355253c24bc
David Mitchell <dmitchell@morpheus.net>
2012-06-16 13:07:29 -0400

    moved and changed aptget dir

--- modules/aptget/manifests/init.pp
+++ modules/aptget/manifests/init.pp
@@ -1,11 +1,11 @@
-class packages::aptget {
+class aptget {
 
   file { '/etc/apt':
     ensure   => directory,
     owner    => root,
     recurse  => true,
     checksum => md5,
-    source   => 'puppet:///modules/packages/apt-get/apt',
+    source   => 'puppet:///modules/aptget/apt',
   }
 
   exec { '/usr/bin/apt-get update':
